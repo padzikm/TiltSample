6c1ecefd351a398693d9bbe357426cc941b4e7ec
Michal Padzik <padzikm@gmail.com>
2023-03-24 12:07:36 +0100

    cdc

---
+++ backend/TiltDemoApi/Migrations/20230324083709_cdc.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TiltDemoApi.Migrations
+{
+    public partial class cdc : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("EXEC sys.sp_cdc_enable_db");
+
+            migrationBuilder.Sql("EXEC sys.sp_cdc_enable_table @source_schema = N'dbo', @source_name = N'FirstData', @role_name = null, @filegroup_name = null, @supports_net_changes = 0");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
