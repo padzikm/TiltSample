165aaa6498f356e7c601b9f3b7bdfbba3bf0d980
Michal Padzik <padzikm@gmail.com>
2023-03-25 03:05:45 +0100

    mongo

--- k8s.sh
+++ k8s.sh
@@ -1,5 +1,5 @@
 
-K3D_FIX_DNS=1 k3d cluster create k3s-default --servers 1 --agents 2 --registry-create demoregistry -p '8080:80@loadbalancer' -p '51437:31437@server:0' -p '5672:30672@server:0' -p '15672:31672@server:0' --k3s-arg '--disable=traefik@server:0'
+K3D_FIX_DNS=1 k3d cluster create k3s-default --servers 1 --agents 2 --registry-create demoregistry -p '8080:80@loadbalancer' -p '51437:31437@server:0' -p '5672:30672@server:0' -p '27017:30017@server:0' -p '15672:31672@server:0' --k3s-arg '--disable=traefik@server:0'
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.3.0/deploy/static/provider/cloud/deploy.yaml
 
